9da1c5d5ad86b095d2523d1687bc4eb74a5ec2e8
zyl-mwy <1909992592@qq.com>
2025-12-17 17:26:43 +0800

    Nothing to Say

--- main/build/plugins/calc/CMakeFiles/add_plugin_autogen_timestamp_deps.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/add_plugin_autogen_timestamp_deps"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/add_plugin_autogen_timestamp_deps.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
